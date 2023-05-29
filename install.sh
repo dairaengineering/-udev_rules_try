eb3441f509d8ae0bff6d11918660ee7b287145fa
Asad <132245936+dairaengineering@users.noreply.github.com>
2023-05-29 12:37:24 +0500

    Create install.sh
    testing

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Copying udev rules..."
+sudo cp ./99-android.rules /etc/udev/rules.d/
+
+echo "Restarting udev with new rules..."
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+echo "Killing all instances of adb..."
+sudo killall adb
+
+echo "Now run 'adb devices' as your user!"
